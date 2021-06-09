cf6933e6fedeff6af5c221b1db0776adbed3edc3
Sook Yoon <83147936+sookyoon@users.noreply.github.com>
2021-06-10 03:55:22 +0800

    Update route_public.go

--- frontend/route_public.go
+++ frontend/route_public.go
@@ -7,6 +7,7 @@ import (
 
 //  index() - home page
 func index(w http.ResponseWriter, r *http.Request) {
+	errlog.Trace.Println("making changes at the remote end")
 	Data := struct {
 		PageName string
 		UserName string
