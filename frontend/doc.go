0bb0e85c43f6d79a2d915395c99acc1cc0ec997b
sookyoon <sookyoon.github.com>
2021-06-17 06:06:56 +0800

    Corrcted typo in doc.go 	modified:   frontend/doc.go

--- frontend/doc.go
+++ frontend/doc.go
@@ -1,5 +1,5 @@
 /*
-This web sever acts a proxy to api server (version 1)
+This web server acts a proxy to api server (version 1)
 It provides the development team the opportunity to consume the RecycleLah! endpoints
 and at the same time, it provides opportunity to do integration testing
 */
